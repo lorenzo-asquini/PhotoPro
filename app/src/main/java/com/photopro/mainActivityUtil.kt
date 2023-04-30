1beb598eeae310777f7d5036744b2c14e2abd1a6
LorenzoAsquini <lorenzo.asquini@studenti.unipd.it>
2023-04-30 15:22:44 +0200

    New UI. Open gallery

--- app/src/main/java/com/photopro/mainActivityUtil.kt
+++ app/src/main/java/com/photopro/mainActivityUtil.kt
@@ -2,8 +2,15 @@ package com.photopro
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+
 
 //File with useful functions for MainActivity
 
@@ -13,6 +20,15 @@ fun cameraPermissionGranted(baseContext : Context) =
 fun microphonePermissionGranted(baseContext : Context) =
     ContextCompat.checkSelfPermission(baseContext, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
 
-fun cameraSelector(baseContext: Context){
+fun openGallery(baseContext : Context) {
+    //Create an intent that opens the default gallery app
+    val intent = Intent(Intent.ACTION_MAIN)
+    //The app must support CATEGORY_APP_GALLERY (most gallery apps do, but there may be some exceptions)
+    intent.addCategory(Intent.CATEGORY_APP_GALLERY)
 
+    //This flag is necessary to open the gallery app as a new task, and not as an activity of this app
+    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+    // Start the default gallery app
+    baseContext.startActivity(intent)
 }
